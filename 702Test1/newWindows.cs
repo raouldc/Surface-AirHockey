722bab2007d01b9e77cc0cb7014f396370e47ad0
Raoul D'Cunha <raoul_dcunha@hotmail.com>
2014-09-27 16:46:20 +1200

    initial commit. displays coordinates of the mouse pointer in a messagebox

---
+++ 702Test1/newWindows.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _702Test1
+{
+    public partial class newWindows : Form
+    {
+        public newWindows()
+        {
+            InitializeComponent();
+        }
+    }
+}
