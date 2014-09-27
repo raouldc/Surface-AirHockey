f56bbd1d2da2f4a803de250c48583d3c8c0b30fd
Raoul D'Cunha <raoul_dcunha@hotmail.com>
2014-09-27 18:28:06 +1200

    made client server

--- JavaServer/702JavaApp/src/ahci/Client.java
+++ JavaServer/702JavaApp/src/ahci/Client.java
@@ -3,30 +3,41 @@ package ahci;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
-
-import javax.swing.JOptionPane;
+import java.util.Date;
 
 /**
  * Trivial client for the date server.
  */
 public class Client {
 
-    /**
-     * Runs the client as an application.  First it displays a dialog
-     * box asking for the IP address or hostname of a host running
-     * the date server, then connects to it and displays the date that
-     * it serves.
-     */
-    public static void main(String[] args) throws IOException {
-        String serverAddress = JOptionPane.showInputDialog(
-            "Enter IP Address of a machine that is\n" +
-            "running the date service on port 9090:");
-        Socket s = new Socket(serverAddress, 9090);
-        BufferedReader input =
-            new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String answer = input.readLine();
-        JOptionPane.showMessageDialog(null, answer);
-        System.exit(0);
-    }
+	/**
+	 * Runs the client as an application.  First it displays a dialog
+	 * box asking for the IP address or hostname of a host running
+	 * the date server, then connects to it and displays the date that
+	 * it serves.
+	 */
+	public static void main(String[] args) throws IOException {
+		ServerSocket listener = new ServerSocket(9090);
+		try {
+			while (true) {
+				Socket socket = listener.accept();
+				try {
+
+					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					String x ="";
+					while((x=in.readLine())!=null){
+						System.out.println(x);
+					}
+				} finally {
+					socket.close();
+				}
+			}
+		}
+		finally {
+			listener.close();
+		}
+	}
 }
